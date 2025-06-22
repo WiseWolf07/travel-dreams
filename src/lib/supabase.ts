3078a403e587a8d982d874c6e527c22f1c0817ff
miguelvelezsk <miguelangel.velez@udea.edu.co>
2025-06-21 21:09:37 -0500

    ADD VARIABLE ENV

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pjatzrunallpdkuchcsj.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqYXR6cnVuYWxscGRrdWNoY3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1NTQ5MjQsImV4cCI6MjA2NjEzMDkyNH0.OWnoomP4EpNkVDJFQfnk2cgywM1LJz8SDm2eku2hmoM"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
